21f61fad3a99d4fdba4d4feb7c20251e335cb36f
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2024-06-21 09:56:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,33 +8,36 @@ import time
 import docx2txt
 
 model_name_dict = {
-        'nllb-distilled-600M': 'facebook/nllb-200-distilled-600M',
-                  #'nllb-1.3B': 'facebook/nllb-200-1.3B',
-                  # 'nllb-distilled-1.3B': 'facebook/nllb-200-distilled-1.3B',
-                  #'nllb-3.3B': 'facebook/nllb-200-3.3B',
-                  }
+    "nllb-distilled-600M": "facebook/nllb-200-distilled-600M",
+    "nllb-distilled-1.3B": "facebook/nllb-200-distilled-1.3B",
+    "nllb-3.3B": "facebook/nllb-200-3.3B",
+}
+
 
 def load_models():
     # build model and tokenizer
     model_dict = {}
 
-    device =  'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     for call_name, real_name in model_name_dict.items():
-        print('\tLoading model: %s' % call_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(real_name, torch_dtype=torch.float32).to(device)
+        print("\tLoading model: %s" % call_name)
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            real_name, torch_dtype=torch.float32
+        ).to(device)
         tokenizer = AutoTokenizer.from_pretrained(real_name)
-        model_dict[call_name+'_model'] = model
-        model_dict[call_name+'_tokenizer'] = tokenizer
+        model_dict[call_name + "_model"] = model
+        model_dict[call_name + "_tokenizer"] = tokenizer
 
     return model_dict
 
+
 def read_file(file):
     # Read content from a file
-    if file.name.endswith('.txt'):
-        with open(file.name, 'r', encoding='utf-8') as f:
+    if file.name.endswith(".txt"):
+        with open(file.name, "r", encoding="utf-8") as f:
             return f.read()
-    elif file.name.endswith('.docx'):
+    elif file.name.endswith(".docx"):
         return docx2txt.process(file.name)
     else:
         return None
@@ -49,7 +52,7 @@ def translation(model_name, source, target, file=None, progress=gr.Progress()):
         "Russian": "rus_Cyrl",
         "Spanish": "spa_Latn",
         "French": "fra_Latn",
-        "Portuguese (Brazil)": "por_Latn"
+        "Portuguese (Brazil)": "por_Latn",
     }
 
     start_time = time.time()
@@ -61,59 +64,63 @@ def translation(model_name, source, target, file=None, progress=gr.Progress()):
         if text is None:
             return {"error": "Unsupported file format"}
 
+    model = model_dict[model_name + "_model"]
+    tokenizer = model_dict[model_name + "_tokenizer"]
 
-    model = model_dict[model_name + '_model']
-    tokenizer = model_dict[model_name + '_tokenizer']
+    translator = pipeline(
+        "translation",
+        model=model,
+        tokenizer=tokenizer,
+        src_lang=source,
+        tgt_lang=target,
+    )
 
-    translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang=source, tgt_lang=target)
-
-    result = ''
+    result = ""
     n = 1
-    for x in text.split('\n\n'):
-      if x.strip() != '':
-        for text_chunk in x.split('.'):
-            print(text_chunk)
-            translation = translator(text_chunk, max_length=512)[0]['translation_text']
-            result += translation   
-        result += '\n\n'
-        n += 1
-        progress(n / len(text.split('\n\n')))
+    for x in text.split("\n\n"):
+        if x.strip() != "":
+            for text_chunk in x.split("."):
+                text_chunk = text_chunk + "."
+                print(text_chunk)
+                translation = translator(text_chunk, max_length=512)[0][
+                    "translation_text"
+                ]
+                result += translation + " "
+            result += "\n\n"
+            n += 1
+            progress(n / len(text.split("\n\n")))
 
     print(result)
 
-    with open('output.txt', 'w', encoding='utf-8') as f:
+    with open("output.txt", "w", encoding="utf-8") as f:
         f.write(result)
-    return 'output.txt'
+    return "output.txt"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     global model_dict
 
     model_dict = load_models()
 
-   # Define Gradio demo
+    # Define Gradio demo
     model_names = model_name_dict.keys()
 
-    source_languages = [
-        "English (US)",
-        "Russian",
-        "Spanish"
-    ]
+    source_languages = ["English (US)", "Russian", "Spanish"]
 
     target_languages = [
         "English (US)",
         "Spanish",
         "French",
         "Russian",
-        "Portuguese (Brazil)"
+        "Portuguese (Brazil)",
     ]
 
     inputs = [
-        gr.Dropdown(model_names, label='NLLB Model', value='nllb-distilled-600M'),
-        gr.Dropdown(source_languages, label='Source Language', value="English (US)"),
-        gr.Dropdown(target_languages, label='Target Language'),
-        gr.File(label="Upload file (txt or docx format)", file_types=['.txt', '.docx']),
+        gr.Dropdown(model_names, label="NLLB Model", value="nllb-distilled-600M"),
+        gr.Dropdown(source_languages, label="Source Language", value="English (US)"),
+        gr.Dropdown(target_languages, label="Target Language"),
+        gr.File(label="Upload file (txt or docx format)", file_types=[".txt", ".docx"]),
     ]
 
     outputs = gr.File()
@@ -126,5 +133,5 @@ if __name__ == '__main__':
         inputs=inputs,
         outputs=outputs,
         title=title,
-        description=description
+        description=description,
     ).launch(debug=True, share=True)
